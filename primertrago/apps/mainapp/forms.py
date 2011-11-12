c4f313d75286383189501155973d995e88abdf63
Sebastian Serrano <sebastian.serrano@gmail.com>
2011-11-12 19:04:46 -0300

    wip

--- primertrago/apps/mainapp/forms.py
+++ primertrago/apps/mainapp/forms.py
@@ -3,13 +3,14 @@ from mainapp.models import Neighborhood
 from django.utils.translation import gettext as _
 
 WEEKDAY = (
+    ('', "--"),
     ('monday', _("Lunes")),
     ('tuesday', _("Martes")),
     ('wednesday', _("Miercoles")),
-    ('jueves', _("Jueves")),
-    ('viernes', _("Viernes")),
-    ('sabado', _("Sabado")),
-    ('domingo', _("Domingo")),
+    ('thursday', _("Jueves")),
+    ('friday', _("Viernes")),
+    ('saturday', _("Sabado")),
+    ('sunday', _("Domingo")),
 )
 class FilterForm(forms.Form):
     weekday = forms.ChoiceField(required=False,choices=WEEKDAY, widget=forms.Select(attrs={'class': 'medium dropdown day'}))
